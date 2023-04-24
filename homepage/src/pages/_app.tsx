77917e85ccbc02a10dd3e7173301f398b8243887
王观伟 <994259213@qq.com>
2023-04-24 16:09:18 +0800

    feat: Add statistics

--- homepage/src/pages/_app.tsx
+++ homepage/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components";
 import MainLayout from "@/layout/MainLayout";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
+import Script from "next/script";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -18,8 +19,16 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return (
-    <ThemeProvider>
-      <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>
-    </ThemeProvider>
+    <>
+      {process.env.NODE_ENV === "production" && (
+        <Script
+          type="text/javascript"
+          src="https://v1.cnzz.com/z_stat.php?id=1281262430&web_id=1281262430"
+        />
+      )}
+      <ThemeProvider>
+        <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>
+      </ThemeProvider>
+    </>
   );
 }
