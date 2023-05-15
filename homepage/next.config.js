6e6e380a6bee50b07f34738de211b91523f3cb18
delong <w.del@qq.com>
2023-05-15 14:26:16 +0800

    feat(homepage): add online detection page
    feat(homepage)
    
    # Conflicts:
    #	homepage/src/layout/MainLayout/Header/index.tsx

--- homepage/next.config.js
+++ homepage/next.config.js
@@ -1,10 +1,32 @@
+const isProduction = process.env.NODE_ENV == "production";
+const isDevelopment = process.env.NODE_ENV == "development";
+
+const prodConfig = {
+  output: "export",
+};
+
+const devConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/poc/:path*",
+        destination:
+          "https://waf-ce.chaitin.cn/api/poc/:path*",
+      },
+    ];
+  },
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
+
+Object.assign(
+  nextConfig,
+  isProduction && prodConfig,
+  isDevelopment && devConfig
+);
 
 module.exports = nextConfig;
