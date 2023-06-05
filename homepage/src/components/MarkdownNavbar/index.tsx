6edc15611eb83c8c9e058b844cd5831bcc8be3e3
王观伟 <994259213@qq.com>
2023-06-05 10:44:23 +0800

    fix: markdown nav title

--- homepage/src/components/MarkdownNavbar/index.tsx
+++ homepage/src/components/MarkdownNavbar/index.tsx
@@ -86,10 +86,10 @@ const MarkdownNavbar: FC<MarkdownNavbarProps> = (props) => {
 
   const getNavStructure: (source: string) => Nav[] = (source) => {
     const contentWithoutCode = source
+      .replace(/```[^`\n]*\n+[^```]+```\n+/g, "")
       .replace(/^[^#]+\n/g, "")
       .replace(/(?:[^\n#]+)#+\s([^#\n]+)\n*/g, "") // 匹配行内出现 # 号的情况
       .replace(/^#\s[^#\n]*\n+/, "")
-      .replace(/```[^`\n]*\n+[^```]+```\n+/g, "")
       .replace(/`([^`\n]+)`/g, "$1")
       .replace(/\*\*?([^*\n]+)\*\*?/g, "$1")
       .replace(/__?([^_\n]+)__?/g, "$1")
