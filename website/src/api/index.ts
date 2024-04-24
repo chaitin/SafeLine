63832e2dcf4f69533ad61535af0168d08fed3bae
bens.CN <993631441@qq.com>
2024-04-24 17:03:43 +0800

    fix: post api

--- website/src/api/index.ts
+++ website/src/api/index.ts
@@ -38,6 +38,6 @@ function detectorPoint(query: { source?: string; type: 1001 | 1002 }) {
   const search = new URLSearchParams(location.search);
   query.source = search.get("source") || document.referrer;
   return fetch(
-    getAPIUrl() + "/api/behavior?" + new URLSearchParams(query as any), {method: 'POST'}
+    getAPIUrl() + "/behavior", {method: 'POST', body: JSON.stringify(query)}
   ).then((res) => res.json());
 }
