8d62dedcc1689c462f68a6e121f516202aa7c404
xiaobing.wang <xiaobing.wang@chaitin.com>
2024-06-18 17:54:38 +0800

    feat: not -qq

--- release/latest/get-docker.sh
+++ release/latest/get-docker.sh
@@ -504,13 +504,13 @@ do_install() {
 				if ! is_dry_run; then
 					set -x
 				fi
-				$sh_c 'apt-get update -qq >/dev/null'
-				$sh_c "DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $pre_reqs >/dev/null"
+				$sh_c 'apt-get update >/dev/null'
+				$sh_c "DEBIAN_FRONTEND=noninteractive apt-get install -y $pre_reqs >/dev/null"
 				$sh_c 'install -m 0755 -d /etc/apt/keyrings'
 				$sh_c "curl -fsSL \"$DOWNLOAD_URL/linux/$lsb_dist/gpg\" -o /etc/apt/keyrings/docker.asc"
 				$sh_c "chmod a+r /etc/apt/keyrings/docker.asc"
 				$sh_c "echo \"$apt_repo\" > /etc/apt/sources.list.d/docker.list"
-				$sh_c 'apt-get update -qq >/dev/null'
+				$sh_c 'apt-get update >/dev/null'
 			)
 			pkg_version=""
 			if [ -n "$VERSION" ]; then
@@ -552,7 +552,7 @@ do_install() {
 				if ! is_dry_run; then
 					set -x
 				fi
-				$sh_c "DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $pkgs >/dev/null"
+				$sh_c "DEBIAN_FRONTEND=noninteractive apt-get install -y $pkgs >/dev/null"
 			)
 			echo_docker_as_nonroot
 			exit 0
